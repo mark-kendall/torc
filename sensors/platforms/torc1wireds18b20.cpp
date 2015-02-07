005830ec439216dae8aa9f1fb2426e1fa692a038
Mark Kendall <mark.kendall@gmail.com>
2015-02-07 16:56:58 +0000

    Finish 1Wire DS18B20 device reading.

--- sensors/platforms/torc1wireds18b20.cpp
+++ sensors/platforms/torc1wireds18b20.cpp
@@ -20,16 +20,113 @@
 * USA.
 */
 
+// Qt
+#include <QFile>
+#include <QTextStream>
+
 // Torc
+#include "torclogging.h"
+#include "torc1wiremonitor.h"
 #include "torc1wireds18b20.h"
+
+Torc1WireReadThread::Torc1WireReadThread(Torc1WireDS18B20 *Parent)
+  : TorcQThread("1Wire"),
+    m_parent(Parent),
+    m_timer(NULL) 
+{   
+}   
+
+void Torc1WireReadThread::Start(void)
+{
+    // create and setup timer in correct thread
+    m_timer = new QTimer();
+    m_timer->setTimerType(Qt::CoarseTimer);
+    m_timer->setInterval(5000);
+    // 'this' is in the parent's thread and connect will by default use an indirect connection,
+    // so force a direct connection to ensure Read operates in the 1Wire thread.
+    connect(m_timer, SIGNAL(timeout()), this, SLOT(Read()), Qt::DirectConnection);
+    m_timer->start();
+}
+
+void Torc1WireReadThread::Finish(void)
+{
+    delete m_timer;
+    m_timer = NULL;
+}
+
+void Torc1WireReadThread::Read(void)
+{
+    QString uniqueId = m_parent->GetUniqueId();
+    QFile file(ONE_WIRE_DIRECTORY + "/" + uniqueId + "/w1_slave");
+    
+    // open
+    if (!file.open(QIODevice::ReadOnly))
+    {   
+        LOG(VB_GENERAL, LOG_ERR, QString("Failed to read device %1").arg(uniqueId));
+    }
+    else
+    {   
+        // read
+        QTextStream text(&file);
+        
+        // check crc 
+        QString line = text.readLine();
+        if (!line.contains("crc") || !line.contains("YES"))
+        {   
+            LOG(VB_GENERAL, LOG_ERR, QString("CRC check failed for device %1").arg(uniqueId));
+        }
+        else
+        {   
+            // read temp
+            line = text.readLine().trimmed();
+            int index = line.lastIndexOf("t=");
+            if (index < 0)
+            {   
+                LOG(VB_GENERAL, LOG_ERR, QString("Failed to parse temperature for device %1").arg(uniqueId));
+            }
+            else
+            {   
+                line = line.mid(index + 2);
+                bool ok = false;
+                double temp = line.toDouble(&ok);
+                if (ok)
+                {
+                    file.close();
+                    m_parent->Read(temp / 1000.0, true);
+                    return;
+                }   
+                
+                LOG(VB_GENERAL, LOG_ERR, QString("Failed to convert temperature for device %1").arg(uniqueId));
+            }   
+        }   
+    }   
+    
+    file.close();
+    m_parent->Read(0, false);
+}
 
 Torc1WireDS18B20::Torc1WireDS18B20(const QString &UniqueId)
   : TorcTemperatureSensor(TorcTemperatureSensor::Celsius, 0, -55.0, 125.0,
-                          DS18B20NAME, UniqueId)
+                          DS18B20NAME, UniqueId),
+    m_readThread(new Torc1WireReadThread(this))
 {
+    m_readThread->start();
 }
 
 Torc1WireDS18B20::~Torc1WireDS18B20()
 {
+    m_readThread->quit();
+    m_readThread->wait();
+    delete m_readThread;
+    m_readThread = NULL;
 }
 
+void Torc1WireDS18B20::Read(double Value, bool Valid)
+{
+    if (Valid)
+        SetValue(Value);
+    else
+        SetValid(false);
+}
+
+           
--- sensors/platforms/torc1wireds18b20.h
+++ sensors/platforms/torc1wireds18b20.h
@@ -1,16 +1,46 @@
 #ifndef TORC1WIREDS18B20_H
 #define TORC1WIREDS18B20_H
 
+// Qt
+#include <QTimer>
+
 // Torc
+#include "torcqthread.h"
 #include "../torctemperaturesensor.h"
 
 #define DS18B20NAME QString("DS18B20")
 
+class Torc1WireReadThread;
+
 class Torc1WireDS18B20 : public TorcTemperatureSensor
 {
+    Q_OBJECT
+
   public:
     Torc1WireDS18B20(const QString &UniqueId);
     ~Torc1WireDS18B20();
+
+    void   Read  (double Value, bool Valid);
+
+  private:
+    Torc1WireReadThread *m_readThread;
 };
 
+
+class Torc1WireReadThread : public TorcQThread
+{
+    Q_OBJECT
+
+  public:
+    Torc1WireReadThread(Torc1WireDS18B20 *Parent);
+    void Start(void);
+    void Finish(void);
+
+  public slots:
+    void Read(void);
+
+  private:
+    Torc1WireDS18B20  *m_parent;
+    QTimer            *m_timer;
+};
 #endif // TORC1WIREDS18B20_H
