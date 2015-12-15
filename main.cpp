2ce883987c8b533530c2b24a3a10fcb43af94624
Mark Kendall <mark.kendall@gmail.com>
2015-12-15 17:00:52 +0000

    Missed in last commit

--- main.cpp
+++ main.cpp
@@ -13,18 +13,18 @@ int main(int argc, char **argv)
     QCoreApplication::setApplicationName("torc");
     QThread::currentThread()->setObjectName(TORC_MAIN_THREAD);
 
-    int ret = GENERIC_EXIT_OK;
+    int ret = TORC_EXIT_OK;
 
     {
         bool justexit = false;
         QScopedPointer<TorcCommandLine> cmdline(new TorcCommandLine(TorcCommandLine::Database | TorcCommandLine::LogFile));
 
         if (!cmdline.data())
-            return GENERIC_EXIT_NOT_OK;
+            return TORC_EXIT_UNKOWN_ERROR;
 
         ret = cmdline->Evaluate(argc, argv, justexit);
 
-        if (ret != GENERIC_EXIT_OK)
+        if (ret != TORC_EXIT_OK)
             return ret;
 
         if (justexit)
