abd14db387a43fbabfd16d7db5cfe7032e4d1e2f
Mark Kendall <mark.kendall@gmail.com>
2015-12-15 21:22:19 +0000

    Remove some unused defines

--- torc/torclocaldefs.h
+++ torc/torclocaldefs.h
@@ -8,13 +8,6 @@
 #define TORC_MAIN_THREAD  QString("MainLoop")
 #define TORC_ADMIN_THREAD QString("AdminLoop")
 
-#define TORC_KEYEVENT_MODIFIERS           (Qt::NoModifier | Qt::ShiftModifier | Qt::ControlModifier | Qt::AltModifier | Qt::MetaModifier | Qt::KeypadModifier)
-#define TORC_KEYEVENT_PRINTABLE_MODIFIERS (Qt::NoModifier | Qt::ShiftModifier | Qt::ControlModifier | Qt::AltModifier | Qt::MetaModifier)
-
 #define TORC_CORE                  QString("CORE_")
-#define TORC_GUI                   QString("GUI_")
-#define TORC_AUDIO                 QString("AUDIO_")
-#define TORC_VIDEO                 QString("VIDEO_")
-#define TORC_DB                    QString("DB_")
 
 #endif // TORCLOCALDEFS_H
