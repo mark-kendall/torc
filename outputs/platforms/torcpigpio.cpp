8bebf8478cbfa30d44c7d571d5a5cc2a18c27f85
Mark Kendall <mark.kendall@gmail.com>
2016-02-08 20:40:18 +0000

    Documentation updates

--- outputs/platforms/torcpigpio.cpp
+++ outputs/platforms/torcpigpio.cpp
@@ -30,6 +30,113 @@
 // wiringPi
 #include "wiringPi.h"
 
+/*!
+* \page pi Torc on the Raspberry Pi
+* \section pitorc Introduction
+*
+* Torc was originally written for installation on a Model B Raspberry Pi but will work on any
+* version of the hardware. It has been tested on the Model B and Zero.
+*
+* A full desktop installation is not required but a network connection is highly recommended (Torc is
+* accessed through a web interface).
+*
+* \note While Torc will run on the compute module, GPIO will not currently work as the pin numbering scheme is different.
+*
+* \note The following instructions are not exhaustive and additional steps may be required for distributions other
+* than Raspbian Jessie.
+*
+* \section piinstall Installing on the Raspberry Pi.
+*
+* \subsection pidownload Download and install linux.
+*  Download the latest version of your favourite Raspberry Pi linux image (the following instructions
+* assume Raspbian Jesie Lite - but any recent distribution should work). Torc does not require a desktop
+* interface so a minimal (headless) version is fine. Install that image on a suitable SD card and insert the card
+* into your device.
+*
+* \subsection piboot Boot device and log in.
+* Boot your device and log in. If you are running a full desktop insallation, open a command terminal -
+* the following assumes you are installing from the command line.
+*
+* \subsection piupdate Update your installation.
+* Update your distribution for all of the latest fixes and releases by typing the following commands
+* (depending on the number of updates, this may take a few minutes):
+* \code
+* sudo apt-get update
+* sudo apt-get upgrade
+* \endcode
+*
+* \subsection piconfig Change your Raspberry Pi configuration.
+* From the command line, run:
+* \code
+* sudo raspi-config
+* \endcode
+*
+* You can change a number of Raspberry Pi specific behaviours from this utility. You will probably
+* want to 'Expand filesystem' (to make sure the entire SD card is available to you), 'Change user password',
+* set the timezone (IMPORTANT) and from the 'Advanced options' screen, 'Set the hostname', 'Enable SSH', 'Enable SPI' and 'Enable I2C'.
+* The last 2 options are dependant on the hardware you intend to attach to your device. If you have no intention of attaching
+* a screen or monitor to your device, you can also allocate zero memory to the GPU.
+*
+* \subsection pidev Install software dependencies.
+*
+* From the command line run:
+* \code
+* sudo apt-get install libgraphviz-dev libavahi-compat-libdnssd-dev qt5-default git-core libqt5xmlpatterns5-dev upower
+* \endcode
+*
+* \subsection piwiringpi Install wiringPi.
+*
+* Torc uses wiringPi to access the Raspberry Pi's GPIO pins and help with I2C communications. It is highly unlikely
+* that you'll want to use Torc on a Pi without wiringPi. Full wiringPi instructions are available at the wiringPi
+* <a href="http://wiringpi.com/download-and-install"> project page</a>.
+*
+* \code
+* cd ~
+* git clone git://git.drogon.net/wiringPi
+* cd wiringPi
+* ./build
+* \endcode
+*
+* \subsection pitorcinstall Download and install Torc.
+* \note This will take some time. Make a few cups of coffee...
+* \code
+* cd ~
+* git clone https://github.com/mark-kendall/torc.git
+* cd torc
+* qmake
+* make -j2
+* sudo make install
+* \endcode
+*
+* \subsection pi1wire Enable 1Wire interface.
+* The 1Wire interface is primarily used for communicating with MAXIM DS18B20 1Wire digital thermometers - which
+* are also available as a waterproof module.
+*
+* Add the following line to the end of '/boot/config.txt' file (sudo nano /boot/config.txt)
+* \code
+* dtoverlay=w1-gpio
+* \endcode
+*
+* and these 2 lines to /etc/modules (sudo nano /etc/modules)
+* \code
+* w1-gpio
+* w1-therm
+* \endcode
+*
+* \note If you enable w1-gpio support without adding the appropriate external hardware (i.e. a pull up
+* resister), you may find the kernel tries to recognise a number of phantom 1Wire devices. Don't panic.
+*
+* \subsection piPower Allow Torc to poweroff/restart your device.
+*
+* \subsection pii2c Enable I2C bus.
+*
+* \subsection pitime Set time and timezone.
+*
+* \subsection pistart Run Torc at startup.
+*
+* \subsection Super user permissions
+*/
+
 TorcPiGPIO* TorcPiGPIO::gPiGPIO = new TorcPiGPIO();
 
 TorcPiGPIO::TorcPiGPIO()
