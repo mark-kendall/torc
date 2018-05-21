332b7224903c22e43cd364cd3b1be1bed0973dc5
Mark Kendall <mark.kendall@gmail.com>
2018-05-21 08:57:28 +0100

    Web interface: Fix Close translation in theme

--- html/js/torctheme.js
+++ html/js/torctheme.js
@@ -41,7 +41,7 @@ var theme = {
 '    <div class="modal-content">' +
 '      <div class="modal-header">' +
 '        <h4 class="modal-title"><%=title%></h4>' +
-'        <button type="button" class="close" data-dismiss="modal" aria-label="' + CloseTr + '">' +
+'        <button type="button" class="close" data-dismiss="modal" aria-label="' + torc.CloseTr + '">' +
 '          <span aria-hidden="true">&times;</span>' +
 '        </button>' +
 '      </div>' +
@@ -49,7 +49,7 @@ var theme = {
 '        <pre id="<%=contentid%>"></pre>' +
 '      </div>' +
 '      <div class="modal-footer">' +
-'        <button type="button" class="btn btn-default" data-dismiss="modal">' + CloseTr + '</button>' +
+'        <button type="button" class="btn btn-default" data-dismiss="modal">' + torc.CloseTr + '</button>' +
 '      </div>' +
 '    </div>' +
 '  </div>' +
@@ -67,13 +67,13 @@ var theme = {
 '    <div class="modal-content">' +
 '      <div class="modal-header">' +
 '        <h4 class="modal-title"><%=title%></h4>' +
-'        <button type="button" class="close" data-dismiss="modal" aria-label="' + CloseTr + '">' +
+'        <button type="button" class="close" data-dismiss="modal" aria-label="' + torc.CloseTr + '">' +
 '          <span aria-hidden="true">&times;</span>' +
 '        </button>' +
 '      </div>' +
 '      <div class="modal-body torc-api-modal-content"></div>' +
 '      <div class="modal-footer">' +
-'        <button type="button" class="btn btn-default" data-dismiss="modal">' + CloseTr + '</button>' +
+'        <button type="button" class="btn btn-default" data-dismiss="modal">' + torc.CloseTr + '</button>' +
 '      </div>' +
 '    </div>' +
 '  </div>' +
