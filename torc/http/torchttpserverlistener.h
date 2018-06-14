be0abcd00f9b779382f1391cc4220306fc3b0b58
Mark Kendall <mark.kendall@gmail.com>
2018-06-14 08:55:13 +0100

    Add TorcHTTPServerListener
    - as yet unused but will allow the server to listen on specific
    addresses.

---
+++ torc/http/torchttpserverlistener.h
@@ -0,0 +1,22 @@
+#ifndef TORCHTTPSERVERADDRESS_H
+#define TORCHTTPSERVERADDRESS_H
+
+// Qt
+#include <QTcpServer>
+
+class TorcHTTPServerListener Q_DECL_FINAL : public QTcpServer
+{
+    Q_OBJECT
+
+  public:
+    TorcHTTPServerListener(QObject *Parent, const QHostAddress &Address, int Port = 0);
+   ~TorcHTTPServerListener();
+
+  signals:
+    void NewConnection(qintptr SocketDescriptor);
+
+  protected:
+    void incomingConnection (qintptr SocketDescriptor) Q_DECL_OVERRIDE Q_DECL_FINAL;
+};
+
+#endif // TORCHTTPSERVERADDRESS_H
