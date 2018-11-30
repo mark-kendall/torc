672f3ba5bc76673222b20b9f59abf633076d3f0c
Mark Kendall <mark.kendall@gmail.com>
2018-11-30 19:22:00 +0000

    TorcCoreUtils: Add QMetaEnum template helper functions
    - and use namespace rather than class

--- torc/torccoreutils.h
+++ torc/torccoreutils.h
@@ -6,15 +6,37 @@
 #include <QMetaEnum>
 #include <QDateTime>
 
-class TorcCoreUtils
+namespace TorcCoreUtils
 {
-  public:
-    static QDateTime   DateTimeFromString    (const QString &String);
-    static quint64     GetMicrosecondCount   (void);
-    static void        QtMessage             (QtMsgType Type, const QMessageLogContext &Context, const QString &Message);
-    static bool        HasZlib               (void);
-    static QByteArray  GZipCompress          (QByteArray &Source);
-    static QByteArray  GZipCompressFile      (QFile &Source);
-};
+    QDateTime   DateTimeFromString    (const QString &String);
+    quint64     GetMicrosecondCount   (void);
+    void        QtMessage             (QtMsgType Type, const QMessageLogContext &Context, const QString &Message);
+    bool        HasZlib               (void);
+    QByteArray  GZipCompress          (QByteArray &Source);
+    QByteArray  GZipCompressFile      (QFile &Source);
+
+    template <typename T> QString EnumToLowerString(T Value)
+    {
+        return QString(QMetaEnum::fromType<T>().valueToKey(Value)).toLower();
+    }
+
+    template <typename T> QString EnumToString(T Value)
+    {
+        return QMetaEnum::fromType<T>().valueToKey(Value);
+    }
+
+    template <typename T> int StringToEnum(const QString &Value, bool CaseSensitive = false)
+    {
+        const QMetaEnum metaEnum = QMetaEnum::fromType<T>();
+        if (CaseSensitive)
+            return metaEnum.keyToValue(Value.toLatin1());
+        QByteArray value = Value.toLower().toLatin1();
+        int count = metaEnum.keyCount();
+        for (int i = 0; i < count; i++)
+            if (qstrcmp(value, QByteArray(metaEnum.key(count)).toLower()) == 0)
+                return metaEnum.value(count);
+        return -1; // for consistency with QMetaEnum::keyToValue
+    }
+}
 
 #endif // TORCCOREUTILS_H
