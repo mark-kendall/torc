b740169732220b18b3322bcc97b1f4d1996f68f1
Mark Kendall <mark.kendall@gmail.com>
2018-12-03 10:50:23 +0000

    Add a clazy build script

---
+++ clazy-build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# exclusions - based on clazy 1.3
+# inefficient-qlist             various QVariant functions only return QVariantList (aka QList<QVariant>). Valid hits fixed.
+# non-pod-global-static         doesn't fair well with factory classes
+# ctor-missing-parent-argument  we simply don't need the QObject parenting in most cases (any?)
+# qstring-allocations           soooo many
+# old-style-connect             have been meaning to fix anyway
+
+make clean
+export CLAZY_CHECKS="level0,level1,level2,no-inefficient-qlist,no-non-pod-global-static,no-ctor-missing-parent-argument,no-qstring-allocations,no-old-style-connect"
+clazy --list
+TORC_FFMPEG=1 qmake -spec linux-clang QMAKE_CXX="clazy"
+make -j8
